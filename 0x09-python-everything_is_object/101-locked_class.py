6f16cd516e2886865975ef67bd7a624ceec23e36
Roseybella <roseybella279@gmail.com>
2023-11-28 20:46:08 -0800

    All advance

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+
+cked Class Module"""
+
+
+class LockedClass:
+    """
+    Class that prevents the user from dynamically creating any new
+    instance attributes, except if new attribute is called `first_name`
+    """
+    __slots__ = ('first_name')
+    pass
